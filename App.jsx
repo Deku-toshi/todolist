b0a5881d99ac4ad701877ffb27f2db704dfb3132
Deku-toshi <liberty3toshi@gmail.com>
2025-05-19 19:15:05 +0900

    todoの入力フォームと追加ボタンを追記

--- App.jsx
+++ App.jsx
@@ -9,10 +9,16 @@ function App() {
     <>
       {todoList.map((todo) => {
         return (
+          <>
           <div key={todo.id}>
             <button onClick={deleteTodo}>削除</button>
             <span>{todo.content}</span>
           </div>
+          <div>
+            <input type="text" />
+            <button>追加</button>
+          </div>
+          </>
         );
       })}
     </>
