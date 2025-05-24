b729c03e63c67d960634fe5814d6c157934c8e58
Deku-toshi <liberty3toshi@gmail.com>
2025-05-24 11:27:10 +0900

    todoリストを表示するコードをApp.jsxから移行

--- components/TodoList.jsx
+++ components/TodoList.jsx
@@ -1,5 +1,14 @@
 const TodoList = () => {
-
-}
+  return (
+    <>
+      {todos.map((todo) => (
+        <div key={todo.id}>
+          <button onClick={() => deleteTodo(todo.id)}>削除</button>
+          <span>{todo.content}</span>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default TodoList;
