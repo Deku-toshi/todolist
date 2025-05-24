f363319d44f36af38d8d95ab645a5ffb76aafa7c
Deku-toshi <liberty3toshi@gmail.com>
2025-05-24 14:07:09 +0900

    Formコンポーネントでの記述内容のため削除

--- App.jsx
+++ App.jsx
@@ -14,10 +14,6 @@ function App() {
   return (
     <>
       <TodoList />
-      <div>
-        <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)}/>
-        <button onClick={createTodo}>追加</button>
-      </div>
     </>
   );
 }
